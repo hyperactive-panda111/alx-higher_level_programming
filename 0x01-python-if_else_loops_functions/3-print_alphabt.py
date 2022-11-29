17d5a0fd992cfc326555500d363c70112f06bb24
Yaw Amo Mensah <amomensah300@gmail.com>
2022-11-29 23:50:00 +0000

    adding 4th task

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(97,123):
-    if i == 101 and 113:
+for i in range(97, 123):
+    if i == 101 and ch == 113:
         continue
     print("{:c}".format(i), end='')
