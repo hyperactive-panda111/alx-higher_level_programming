e5a1455b35cd278249e89fef10c2aeb398f0d3ac
Yaw Amo Mensah <amomensah300@gmail.com>
2022-11-29 21:59:23 +0000

    adding first task

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,4 +1,9 @@
 #!/bin/python3
 import random
 number = random.randint(-10, 10)
-print(f"{number}")
+if number > 0:
+    print(f"{number:d} is positive")
+elif number == 0:
+    print(f"{number:d} is zero")
+else:
+    print(f"{number:d} is negative")
