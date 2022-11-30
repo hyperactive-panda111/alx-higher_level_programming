0c7bd402febf66463d3f19094503071398f4e402
Yaw Amo Mensah <amomensah300@gmail.com>
2022-11-30 02:10:13 +0000

    adding 9th task

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    for i in range(len(number)):
+        if ord(number[i] >= 97 and ord(number[i]) <= 122:
+                num = 32
+        else:
+                num = 0
+        print("{:c}".format(ord(number[i] - num), end='')
+    print()
