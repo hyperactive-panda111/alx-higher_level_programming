064471ec614d42ed04a7fafd30986f4811cda94b
Yaw Amo Mensah <amomensah300@gmail.com>
2022-11-30 20:09:23 +0000

    adding test

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,10 +2,12 @@
 def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0:
-            print("Fizz ",end='')
+            print("Fizz ", end='')
         elif i % 5 == 0:
             print("Buzz ", end='')
         elif i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz ", end='')
         else:
             print(i, end='')
+
+        print(" ", end=' ')
