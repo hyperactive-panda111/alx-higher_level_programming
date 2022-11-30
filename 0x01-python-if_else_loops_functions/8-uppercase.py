3b3b5fcff83d6284a77ed4935e2021827a4aaf45
Yaw Amo Mensah <amomensah300@gmail.com>
2022-11-30 02:12:43 +0000

    adding 9th task

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def print_last_digit(number):
-    for i in range(len(number)):
-        if ord(number[i] >= 97 and ord(number[i]) <= 122:
-                num = 32
+def uppercase(str):
+    for i in range(len(str)):
+        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
+            num = 32
         else:
-                num = 0
-        print("{:c}".format(ord(number[i] - num), end='')
+            num = 0
+        print("{:c}".format(ord(str[i]) - num), end='')
     print()
