78987bd95c2479d603d97ccc3bd29ae416752282
Yaw Amo Mensah <amomensah300@gmail.com>
2022-12-02 01:18:27 +0000

    adding 2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ = "__main__":
+    import sys
+    argv = sys.argv[1:]
+    argv_cnt = len(argv)
+    index = 1
+    if argv_cnt is 0:
+        print("{:d} arguments.".format(argv_cnt))
+    elif arg_cnt is 1:
+        print("{:d} argument:".format(argv_cnt))
+        print("{:d}: {:s}".format(index, sys.argv[1]))
+    else:
+        print("{:d} arguments:".format(argv_cnt))
+        while index <= argv_cnt:
+            print("{:d}: {:s}".format(index, sys.argv[index]))
+            index += 1
