d66282a510e5e6e354b2730f160e2ed2fa742fad
Amo Mensah <amomensah300@gmail.com>
2023-02-16 00:27:29 +0000

    adding task 5

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
---script that prints full description of table
---first_table
+-- Prints the full description of the table first_table from the database
+-- Query to print full description of a table
 SHOW CREATE TABLE first_table;
