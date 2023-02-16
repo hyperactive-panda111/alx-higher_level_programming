1588db6b773aa555843cb286854763852404deb4
Amo Mensah <amomensah300@gmail.com>
2023-02-16 00:08:06 +0000

    added permission to task 4

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,5 @@
---sql script to create table called 'first_table'
+-- Creates a table in the current database
+-- Query to create a table called first_table in the current database in your MySQL server
 CREATE TABLE IF NOT EXISTS first_table (
-id INT, 
+id INT,
 name VARCHAR(256));
