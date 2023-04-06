cf0167b05968b6b5b72fd37cba35846f45fe5e77
Amo Mensah <amomensah300@gmail.com>
2023-04-06 18:21:44 +0000

    adding corrected files for 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ Python script to fetch https://alx-intranet.hbtn.io/status"""
 
-import urllib
+import urllib.request
 
 if __name__ == '__main__':
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        body = response.read()
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        body = res.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
